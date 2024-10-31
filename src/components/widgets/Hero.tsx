fc0d1e1d3c1638e67b1eacdcdbb6a6141b074abe
James Pike <jamesandrewpike@gmail.com>
2024-10-31 10:30:48 -0400

    message

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -11,13 +11,14 @@ export default component$(() => {
         <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
         <div class="py-12 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
           <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
-           <h1 class="text-5xl md:text-6xl font-bold leading-tight tracking-tighter mb-4 font-heading dark:text-gray-200">
+          <h1 class="text-6xl md:text-6xl font-bold leading-none tracking-tighter mb-4 font-heading dark:text-gray-200">
   Experienced
   <br />
   <span>Family Lawyer</span>
   <br />
   <span>in Toronto</span>
 </h1>
+
             <div class="max-w-3xl mx-auto lg:max-w-none">
               <p class="text-xl text-muted mb-6 dark:text-slate-300">
                 <span class="font-semibold underline decoration-wavy decoration-1 decoration-secondary-600 underline-offset-2">
